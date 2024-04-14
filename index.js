d0e79053ae2f7ac5adf79677edd7d69b2916f8fa
rahuls49 <rahulsahu79998@gmail.com>
2024-04-14 16:21:09 +0530

    fixed index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 
 // const port = process.env.PORT || 8100;
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const URI  = process.env.URI;
 // MiddleWares
 app.use(express.json())
